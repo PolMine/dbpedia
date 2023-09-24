fa401570c267b9b9767f9de1fcaf59e411b362c7
Andreas Blätte <andreasblatte@MBP-von-Andreas.fritz.box>
2023-09-24 22:00:31 +0200

    DESCRIPTION Remotes: field for SPARQL/polmineR dev

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,9 @@
   if (status[["docker"]]){
     packageStartupMessage("* DBpedia Spotlight Docker container running")
   } else {
-    packageStartupMessage("* Using DBpedia Spotlight online API")
+    packageStartupMessage(
+      "* Using DBpedia Spotlight online API (no local Docker container running)"
+    )
   }
   packageStartupMessage(sprintf("* endpoint/api: %s", status[["endpoint"]]))
   packageStartupMessage(sprintf("* language: %s", status[["lang"]]))
