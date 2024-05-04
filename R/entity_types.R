f176912dee77eb42363f708bed80f41ac36977e7
Andreas Blätte <andreasblatte@MBP-von-Andreas.fritz.box>
2024-05-04 20:52:56 +0200

    some linting of R/entity_types.R

--- R/entity_types.R
+++ R/entity_types.R
@@ -1,3 +1,34 @@
+# Auxillary function to make c("PERSON" = "DBpedia:Person") to a list
+# representation like list("DBpedia" = c("PERSON" = "Person")).
+
+entity_types_categorize <- function(x, mapping_vector) {
+  
+  raw_type_src <- unname(gsub("(.*?)\\:.*$", "\\1", mapping_vector))
+  raw_type <- unname(gsub(".*?\\:(.*)$", "\\1", mapping_vector))
+  category <- names(mapping_vector)
+  
+  if (!all(grepl("\\:", mapping_vector)) | !all(paste0(unique(raw_type_src), "_type") %in% colnames(x))) {
+    cli_abort(c(
+      "When processing `data.table` objects, the `mapping_vector` must include the name of the type source.",
+      "x" = "Either no type source was provided or the provided type source does not correspond to the columns in the data.table."
+    ))
+  }
+  
+  raw_type_src_unique <- unique(raw_type_src)
+  
+  mapping_per_source <- lapply(raw_type_src_unique, function(el) {
+    el_type_idx <- which(el == raw_type_src)
+    raw_type_by_el <- raw_type[el_type_idx]
+    names(raw_type_by_el) <- category[el_type_idx]
+    return(raw_type_by_el)
+  }
+  )
+  
+  names(mapping_per_source) <- paste0(raw_type_src_unique, "_type")
+  return(mapping_per_source)
+}
+
+
 #' Map types returned by DBpedia Spotlight to a limited set of categories
 #'
 #' This function takes the output of `get_dbpedia_uris()` and compares values in
@@ -37,11 +68,11 @@ setGeneric(
 #' @rdname entity_types_map
 #' @examples
 #' library(quanteda)
-#' 
+#'
 #' inaugural_paragraphs <- data_corpus_inaugural %>%
 #'   corpus_subset(Year == 2009) %>% # limit to Barack Obama 2009
 #'   corpus_reshape(to = "paragraphs")
-#'   
+#'
 #' uritab_paragraphs <- get_dbpedia_uris(
 #'   x = inaugural_paragraphs[2], # single paragraph
 #'   language = "en",
@@ -90,7 +121,9 @@ setMethod(
     }
 
     if (!is.character(other) | length(other) > 1) {
-      stop(format_error("{.var other} not character vector of length {.val 1}."))
+      stop(
+        format_error("{.var other} not character vector of length {.val 1}.")
+      )
     }
 
     if (!is.character(x)) {
@@ -121,15 +154,17 @@ setMethod(
 setMethod(
   "entity_types_map", "data.table",
   function(x, mapping_vector, other = "MISC", verbose = TRUE) {
-
+    
     if (!inherits(x, "data.table")) {
       cli_abort("`x` must be a `data.table` object.")
     }
-
+    
     # reorganize mapping vector to match columns
-    mapping_per_source <- entity_types_categorize(x,
-                                                  mapping_vector = mapping_vector)
-
+    mapping_per_source <- entity_types_categorize(
+      x,
+      mapping_vector = mapping_vector
+    )
+    
     # check whether mapping vector is valid
     for (mapping_idx in seq_along(mapping_per_source)) {
       mult_mapping <- which(table(mapping_per_source[[mapping_idx]]) > 1)
@@ -142,76 +177,52 @@ setMethod(
         )
       }
     }
-
+    
     category_per_source <- sapply(seq_along(mapping_per_source), function(i) {
       type_src <- names(mapping_per_source)[[i]]
-      categories <- entity_types_map(x = x[[type_src]],
-                                     mapping_vector = mapping_per_source[[i]],
-                                     other = other,
-                                     verbose = TRUE)
+      categories <- entity_types_map(
+        x = x[[type_src]],
+        mapping_vector = mapping_per_source[[i]],
+        other = other,
+        verbose = TRUE
+      )
     }
     )
-
+    
     # Depending on the number of type sources, this is either a vector or a matrix.
     # Multiple mappings are uniqued and concatenated.
-
+    
     if (length(mapping_per_source) > 1) {
-      categories <- apply(X = category_per_source, MARGIN = 1, function(category) {
-
+      categories <- apply(
+        X = category_per_source,
+        MARGIN = 1,
+        function(category) {
           # in case that there is an ambiguous mapping earlier, there already
           # could be concatenated entities here. Split up.
-
+          
           category <- unlist(strsplit(category, split = "\\|"))
-
+          
           # now unique, sort and concatenate again
           paste0(sort(unique(category)), collapse = "|")
-
-      }
+        }
       )
     } else {
       categories <- as.vector(category_per_source)
     }
-
+    
     # check
     if (length(categories) != nrow(x)) {
-      cli_abort("Mapped categories are not equal to all observations in the input `data.table`.")
+      cli_abort(
+        "Mapped categories are not equal to all observations in the input `data.table`."
+      )
     }
-
+    
     if (verbose)
       cli_alert_info(
         "mapping values in column{?s} {names(mapping_per_source)} to new column {.var category}"
       )
-
+    
     x[, "category" := categories]
     x
-})
+  })
 
-# Auxillary function to make c("PERSON" = "DBpedia:Person") to a list
-# representation like list("DBpedia" = c("PERSON" = "Person")).
-
-entity_types_categorize = function(x, mapping_vector) {
-
-  raw_type_src <- unname(gsub("(.*?)\\:.*$", "\\1", mapping_vector))
-  raw_type <- unname(gsub(".*?\\:(.*)$", "\\1", mapping_vector))
-  category <- names(mapping_vector)
-
-  if (!all(grepl("\\:", mapping_vector)) | !all(paste0(unique(raw_type_src), "_type") %in% colnames(x))) {
-    cli_abort(c(
-      "When processing `data.table` objects, the `mapping_vector` must include the name of the type source.",
-      "x" = "Either no type source was provided or the provided type source does not correspond to the columns in the data.table."
-    ))
-  }
-
-  raw_type_src_unique <- unique(raw_type_src)
-
-  mapping_per_source <- lapply(raw_type_src_unique, function(el) {
-    el_type_idx <- which(el == raw_type_src)
-    raw_type_by_el <- raw_type[el_type_idx]
-    names(raw_type_by_el) <- category[el_type_idx]
-    return(raw_type_by_el)
-  }
-  )
-
-  names(mapping_per_source) <- paste0(raw_type_src_unique, "_type")
-  return(mapping_per_source)
-}
